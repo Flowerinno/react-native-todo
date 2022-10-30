4b98a6820fcacbfbac8a47fe2e218ace433d93a4
Kononov <akellastoopi@gmail.com>
2022-10-30 17:25:56 +0300

    add header component

---
+++ data.js
@@ -0,0 +1,27 @@
+export let data = [
+	{
+		title: "Some news",
+		content: "lorem ipsum dolor sit amet, consectetur adip",
+		id: "1",
+	},
+	{
+		title: "Some news",
+		content: "lorem ipsum dolor sit amet, consectetur adip",
+		id: "2",
+	},
+	{
+		title: "Some news",
+		content: "lorem ipsum dolor sit amet, consectetur adip",
+		id: "3",
+	},
+	{
+		title: "Some news",
+		content: "lorem ipsum dolor sit amet, consectetur adip",
+		id: "4",
+	},
+	{
+		title: "Some news",
+		content: "lorem ipsum dolor sit amet, consectetur adip",
+		id: "5",
+	},
+];
