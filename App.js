42a376d16e2df296d85cc5d7fff29f4776309548
Kononov <akellastoopi@gmail.com>
2022-10-30 17:46:08 +0300

    add addTodo component

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import { useState } from "react";
 import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 export default function App() {
 	const [todos, setTodos] = useState([
 		{ text: "buy coffee", key: "1" },
 		{ text: "buy milk", key: "2" },
 		{ text: "buy water", key: "3" },
 	]);
-	const renderItem = ({ item }) => {
-		return <Text>{item.text}</Text>;
+	const deleteItem = (key) => {
+		setTodos((prev) => prev.filter((item) => item.key !== key));
 	};
-
+	const renderItem = ({ item }) => {
+		return <TodoItem item={item} deleteItem={deleteItem} />;
+	};
+	const addTodoHandler = (text) => {
+		if (text.length > 3) {
+			setTodos((prev) => [{ text, key: Math.random().toString() }, ...prev]);
+		} else if (text.length < 3) {
+			Alert.alert("OOPS", "Todos must be at least 3 characters long", [
+				{ text: "Understood", onPress: () => console.log("alert closed") },
+			]);
+		}
+	};
 	return (
 		<View style={styles.container}>
 			<Header />
 			<View style={styles.content}>
+				<AddTodo addTodoHandler={addTodoHandler} />
 				<View style={styles.list}>
 					<FlatList data={todos} renderItem={renderItem} />
 				</View>
---
+++ components/addTodo.js
@@ -0,0 +1,34 @@
+import { StyleSheet, TextInput, Button, Text, View } from "react-native";
+import { useState } from "react";
+
+export default function AddTodo({ addTodoHandler }) {
+	const [text, setText] = useState("");
+	const changeHandler = (e) => {
+		setText(e);
+	};
+	return (
+		<View>
+			<TextInput
+				style={styles.input}
+				placeholder="new todo..."
+				onChangeText={changeHandler}
+				value={text}
+			/>
+			<Button
+				onPress={() => addTodoHandler(text)}
+				title="add todo"
+				color="coral"
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	input: {
+		marginBottom: 10,
+		paddingHorizontal: 8,
+		paddingVertical: 6,
+		borderBottomWidth: 1,
+		borderBottomColor: "#ddd",
+	},
+});
